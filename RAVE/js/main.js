d094098de4800e2dd6be6b8b0015c2eaed391c08
Tornsoulcage <scottjoseph96@gmailcom>
2018-04-02 13:35:53 -0500

    Work on sorting vehicle alert table

--- RAVE/js/main.js
+++ RAVE/js/main.js
@@ -106,6 +106,42 @@ document.addEventListener("click", function (e) {
 });
 }
 
+//Sorts the vehicle alerts table
+function sortTable() {
+	  var table, rows, switching, i, x, y, shouldSwitch;
+	  table = document.getElementById("vehicleAlerts");
+	  switching = true;
+	  /* Make a loop that will continue until
+	  no switching has been done: */
+	  while (switching) {
+	    // Start by saying: no switching is done:
+	    switching = false;
+	    rows = table.getElementsByTagName("TR");
+	    /* Loop through all table rows (except the
+	    first, which contains table headers): */
+	    for (i = 1; i < (rows.length - 1); i++) {
+	      // Start by saying there should be no switching:
+	      shouldSwitch = false;
+	      /* Get the two elements you want to compare,
+	      one from current row and one from the next: */
+	      x = rows[i].getElementsByTagName("TD")[0];
+	      y = rows[i + 1].getElementsByTagName("TD")[0];
+	      // Check if the two rows should switch place:
+	      if (x.innerHTML > y.innerHTML) {
+	        // I so, mark as a switch and break the loop:
+	        shouldSwitch= true;
+	        break;
+	      }
+	    }
+	    if (shouldSwitch) {
+	      /* If a switch has been marked, make the switch
+	      and mark that a switch has been done: */
+	      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+	      switching = true;
+	    }
+	  }
+}
+
 //Logs the user into our system
 function login(){
 	//Gathering the info need from the page
@@ -268,6 +304,7 @@ function setUserRights(page){
 				}
 				if(page == 'index'){
 					selectVehicleMileageAlerts();
+					sortTable();
 				}
 				if(page == 'viewfleet'){
 					autocompleteVehicleId("editVehicleIDInput");
@@ -469,8 +506,6 @@ function selectVehicleMileageAlerts(){
 	function display_data(){
 		if(xhr.readyState == 4){
 			if(xhr.status == 200){
-				alert(xhr.responseText);
-
 				var jsonarray = JSON.parse(xhr.responseText);
 				//If our result doesnt contain any elements than we got an empty table
 				if(jsonarray.length != 0){
